69d7b06ff287fd3d158d06c73d35b6b6680126af
siggioa@gmail.com <siggioa@gmail.com@05d01639-0da2-2ae6-8c9e-c7b8ae84bb1e>
2013-03-23 19:04:20 +0000

    added a method for changing the verbosity of the Log mechanism

--- MCBMiniServer/src/mcbmini/utils/Log.java
+++ MCBMiniServer/src/mcbmini/utils/Log.java
@@ -8,13 +8,34 @@ import java.util.Date;
  * @date Mar 22, 2013
  */
 public class Log {
+
+	public enum LogPolicy {EVERYTHING, ONLY_ERRORS, NOTHING};
+
+	private static LogPolicy logPolicy = LogPolicy.EVERYTHING;
+
+	public static void setLogPolicy(LogPolicy policy){
+		logPolicy = policy;
+	}
+
+	public static LogPolicy getLogPolicy(){
+		return logPolicy;
+	}
+
 	public static void println(String str){
 		println(str, false);
 	}
 
 	public static void println(String str, boolean error){
-		if( error ) System.err.println(formatString(str));
-		else System.out.println(formatString(str));
+		if( error ){
+			if( logPolicy != LogPolicy.NOTHING ){
+				System.err.println(formatString(str));
+			}
+		}
+		else{
+			if( logPolicy == LogPolicy.EVERYTHING ){
+				System.out.println(formatString(str));
+			}
+		}
 	}
 
 	protected static String formatString(String str){
